877ea2a85a695e6b34d324345fc82e8bf8b7dced
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-08-09 10:51:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+function countWords() {
+  const text = document.getElementById("evaluatedText").value;
+  const words = text.split(/\s+/).filter(word => word.length > 0).length;
+  document.getElementById("wordCount").textContent = words;
+}
